afedd09e1f2df264e84af101d65d7e791bf99cb1
yaodehuang <sef1548@gmail.com>
2020-09-28 00:40:01 +0800

    feat:  修改成顯示Tour of Heroes Windstorm
    參考: https://angular.tw/guide/displaying-data#showing-component-properties-with-interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero}}</h2>
+    `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularLab';
+  title = 'Tour of Heroes';
+  myHero = 'Windstorm';
 }
